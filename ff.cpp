2c81def667b9fb301c5afcc6f289ddd3c869c263
Nymul Hossain <134966524+Nymul123@users.noreply.github.com>
2024-10-19 11:00:33 +0600

    Add files via upload

---
+++ ff.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+int smallest(int arr[], int size)
+{
+    int small = arr[0]; // Assume the first element is the smallest
+    // Iterate through the array
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] < small)
+        {
+            small = arr[i]; // Update if a smaller element is found
+        }
+    }
+
+    return small;
+}
+
+int main()
+{
+    int count=0;
+    
+    int size,i;
+    cin>>size;
+    int a[size];
+    for(i=0;i<size;i++)
+    {
+    cin>>a[i];
+    }
+
+    int s = smallest(a, size); // Pass the array and its size
+    //cout << s << endl;      // Output the smallest element
+    for (int i = 0; i <size; i++)
+    {
+        if (a[i] == s)
+        {
+            count++;
+        }
+    }
+
+    // Check if the frequency is odd or even
+    if (count % 2 == 1)
+    {
+        cout << "Lucky" << endl;
+    }
+    else
+    {
+        cout << "Unlucky" << endl;
+    }
+
+    return 0;
+}
+   
